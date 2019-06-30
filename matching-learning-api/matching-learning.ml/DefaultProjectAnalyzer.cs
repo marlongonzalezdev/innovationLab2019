9b2b2c554b824bd2cf27e32524f91eddbbf0a05b
Marlon Gonzalez <marlongonzalezdev@gmail.com>
2019-06-30 16:59:59 -0300

    TRaining method

--- matching-learning-api/matching-learning.ml/DefaultProjectAnalyzer.cs
+++ matching-learning-api/matching-learning.ml/DefaultProjectAnalyzer.cs
@@ -1,8 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using matching_learning.ml.Domain;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+using Microsoft.ML.Transforms;
 
 namespace matching_learning.ml
 {
@@ -16,6 +21,7 @@ namespace matching_learning.ml
 
         public Task<RecommendationResponse> GetRecommendationsAsync(RecommendationRequest recommendationRequest)
         {
+            RecommendationModelTraining();
             var candidates = new List<Candidate>
                 {
                     new Candidate
@@ -60,6 +66,54 @@ namespace matching_learning.ml
             {
                 Matches = candidates
             });
+        }
+
+        private void RecommendationModelTraining()
+        {
+            var inputPath = Path.Combine(Environment.CurrentDirectory, "Data", "user-languages.csv");
+            var modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "trainedModel.zip");
+
+            try
+            {
+                var mLContext = new MLContext();
+                //training data, loading data from csv file data file in memory
+
+                var trainDataView = mLContext.Data.LoadFromTextFile("inputPath", 
+                    columns: new[]
+                    {
+                        new TextLoader.Column("Features", DataKind.Single, new[] {new TextLoader.Range(0, 1440) }),
+                        new TextLoader.Column(nameof(Candidate.UserId), DataKind.String, 0)
+                    },
+                    hasHeader: true,
+                    separatorChar: ',');
+
+                //Configure data transformations in pipeline
+                var dataProcessPipeline = mLContext.Transforms.ProjectToPrincipalComponents(outputColumnName: "Score", inputColumnName: "Features", rank: 2)
+                    .Append(mLContext.Transforms.Categorical.OneHotEncoding(outputColumnName: "UserId", inputColumnName: nameof(Candidate.UserId), OneHotEncodingEstimator.OutputKind.Indicator));
+
+                //Create the training pipeline
+                var trainer = mLContext.Clustering.Trainers.KMeans(featureColumnName: "Features", numberOfClusters: 3);
+                var trainingPipeline = dataProcessPipeline.Append(trainer);
+
+                //Train the model fitting to the pivotDataView
+                Console.WriteLine("=============== Training the model ===============");
+                ITransformer trainedModel = trainingPipeline.Fit(trainDataView);
+
+                //STEP 5: Evaluate the model and show accuracy stats
+                Console.WriteLine("===== Evaluating Model's accuracy with Test data =====");
+                var predictions = trainedModel.Transform(trainDataView);
+                var metrics = mLContext.Clustering.Evaluate(predictions, scoreColumnName: "Score", featureColumnName: "Features");
+
+                // Save/persist the trained model to a .ZIP file
+                mLContext.Model.Save(trainedModel, trainDataView.Schema, modelPath);
+
+                Console.WriteLine("The model is saved to {0}", modelPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
     }
 }
