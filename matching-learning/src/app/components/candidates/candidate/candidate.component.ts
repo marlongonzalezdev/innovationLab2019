71a135b8b04c6f1be245ff8e0fefaf382a9e269c
Marlon Gonzalez <marlongonzalezdev@gmail.com>
2019-08-17 01:06:11 -0300

    Update candidates view

--- matching-learning/src/app/components/candidates/candidate/candidate.component.ts
+++ matching-learning/src/app/components/candidates/candidate/candidate.component.ts
@@ -10,6 +10,9 @@ export class CandidateComponent implements OnInit {
 
   constructor(private service: CandidateService) { }
 
+  deliveryUnits = [{ id: 1, value: 'Dep 1' },
+    { id: 2, value: 'Dep 2' },
+    { id: 3, value: 'Dep 3' }];
 
   ngOnInit() {
   }
