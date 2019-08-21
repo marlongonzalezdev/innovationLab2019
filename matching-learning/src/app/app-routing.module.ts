d84f88002e01d8688c8ad156a8da09281d746991
Marlon Gonzalez Gongora <marlongonzalezdev@gmail.com>
2019-08-21 12:24:55 -0300

    Add slider to input criteria component

--- matching-learning/src/app/app-routing.module.ts
+++ matching-learning/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {SkillsComponent} from './components/skills/skills.component';
 import {CandidatesComponent} from './components/candidates/candidates.component';
+import { InputCriteriaComponent } from './components/input-criteria/input-criteria.component';
 
 const routes: Routes = [
   {path: 'skills', component: SkillsComponent},
-  {path: 'candidates', component: CandidatesComponent}
+  {path: 'candidates', component: CandidatesComponent},
+  {path: 'build', component: InputCriteriaComponent},
 ];
 
 @NgModule({
