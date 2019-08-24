44f6274b53ec130935250573217e7e3e73f82165
Pablo Queirolo <pablo_queirolo@tcenergy.com>
2019-08-24 16:34:10 -0300

    Project requeriment filter weight normalized to 1.

---
+++ matching-learning-api/matching-learning.common/Domain/BusinessLogic/ProjectCandidateRequirementHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using matching_learning.common.Domain.DTOs;
+
+namespace matching_learning.common.Domain.BusinessLogic
+{
+    public static class ProjectCandidateRequirementHelper
+    {
+        // The sum of Weight in SkillFilter list should be 1. 
+        public static void Normalize(this ProjectCandidateRequirement pcr)
+        {
+            if ((pcr == null) || (pcr.SkillsFilter == null) || (pcr.SkillsFilter.Count == 0)) { return; }
+
+            var totalWeight = pcr.SkillsFilter.Sum(sf => sf.Weight);
+
+            if (totalWeight == 1M) { return; }
+
+            foreach (var sf in pcr.SkillsFilter)
+            {
+                sf.Weight /= totalWeight;
+            }
+        }
+    }
+}
