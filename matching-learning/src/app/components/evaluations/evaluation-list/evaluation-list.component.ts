8d6732b857dc69410b9c1f809a5bf50b6106c4dd
Marlon Gonzalez <marlongonzalezdev@gmail.com>
2019-08-27 00:53:23 -0300

    Sending candidate id via router link

--- matching-learning/src/app/components/evaluations/evaluation-list/evaluation-list.component.ts
+++ matching-learning/src/app/components/evaluations/evaluation-list/evaluation-list.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-evaluation-list',
   templateUrl: './evaluation-list.component.html',
   styleUrls: ['./evaluation-list.component.css']
 })
-export class EvaluationListComponent implements OnInit {
+export class EvaluationListComponent implements OnInit, OnDestroy {
+  id: number;
+  private sub: any;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.sub = this.route.params.subscribe(params => {
+      this.id = +params.id; // (+) converts string 'id' to a number
+      console.log(this.id);
+      // In a real app: dispatch action to load the details here.
+    });
   }
 
+  ngOnDestroy() {
+    this.sub.unsubscribe();
+  }
 }
