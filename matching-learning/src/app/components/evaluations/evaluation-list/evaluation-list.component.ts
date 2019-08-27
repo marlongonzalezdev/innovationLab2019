142ad108f51619ddd8b2b54dc356d9df3861a527
Marlon Gonzalez <marlongonzalezdev@gmail.com>
2019-08-27 01:04:20 -0300

    Adding logic to get all evaluations by candidate

--- matching-learning/src/app/components/evaluations/evaluation-list/evaluation-list.component.ts
+++ matching-learning/src/app/components/evaluations/evaluation-list/evaluation-list.component.ts
@@ -1,5 +1,9 @@
 import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
+import {EvaluationService} from '../../../shared/services/evaluation.service';
+import {Candidate} from '../../../shared/models/candidate';
+import {Evaluation} from '../../../shared/models/evaluation';
+import {MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-evaluation-list',
@@ -9,14 +13,22 @@ import {ActivatedRoute} from '@angular/router';
 export class EvaluationListComponent implements OnInit, OnDestroy {
   id: number;
   private sub: any;
+  evaluations: Evaluation[] = [];
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(private route: ActivatedRoute, private  evaluationService: EvaluationService) {}
 
   ngOnInit() {
     this.sub = this.route.params.subscribe(params => {
       this.id = +params.id; // (+) converts string 'id' to a number
       console.log(this.id);
-      // In a real app: dispatch action to load the details here.
+      this.evaluationService.getEvaluations(this.id)
+        .subscribe(response => {
+          this.evaluations = response;
+          // this.dataSource = new MatTableDataSource<Candidate>(this.candidates);
+          // this.dataSource.sort = this.sort;
+          // this.dataSource.paginator = this.paginator;
+          console.log(this.evaluations);
+        });
     });
   }
 
