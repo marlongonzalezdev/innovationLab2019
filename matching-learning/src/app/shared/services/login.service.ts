4258ac97d172cbd20416aaa3e5b8ac28d472be8b
Yanara <Yanara.Valdez.Contractor@bydeluxe.com>
2019-08-28 21:13:02 -0300

    Login component

--- matching-learning/src/app/shared/services/login.service.ts
+++ matching-learning/src/app/shared/services/login.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,9 @@ import { Injectable } from '@angular/core';
 export class LoginService {
 
   constructor() { }
+
+  loginForm = new FormGroup ({
+    user: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required)
+  });
 }
