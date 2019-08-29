50d189733e04d79e9c9b0f1324253421ae782d5d
Delia Alvarez <dalvarez@targethealth.com>
2019-08-29 17:11:42 -0300

    KMeans working fine!!!!

--- matching-learning-api/matching-learning-algorithm/Domain/DataModel.cs
+++ matching-learning-api/matching-learning-algorithm/Domain/DataModel.cs
@@ -9,453 +9,453 @@ namespace matching_learning_algorithm.Domain
         public string CandidateId;
 
         [LoadColumn(1)]
-        public float attr1 { get; set; }
+        public float attr1;
 
         [LoadColumn(2)]
-        public float attr2 { get; set; }
+        public float attr2;
 
         [LoadColumn(3)]
-        public float attr3 { get; set; }
+        public float attr3;
 
         [LoadColumn(4)]
-        public float attr4 { get; set; }
+        public float attr4;
 
         [LoadColumn(5)]
-        public float attr5 { get; set; }
+        public float attr5;
 
         [LoadColumn(6)]
-        public float attr6 { get; set; }
+        public float attr6;
 
         [LoadColumn(7)]
-        public float attr7 { get; set; }
+        public float attr7;
 
         [LoadColumn(8)]
-        public float attr8 { get; set; }
+        public float attr8;
 
         [LoadColumn(9)]
-        public float attr9 { get; set; }
+        public float attr9;
 
         [LoadColumn(10)]
-        public float attr10 { get; set; }
+        public float attr10;
 
         [LoadColumn(11)]
-        public float attr11 { get; set; }
+        public float attr11;
 
         [LoadColumn(12)]
-        public float attr12 { get; set; }
+        public float attr12;
 
         [LoadColumn(13)]
-        public float attr13 { get; set; }
+        public float attr13;
 
         [LoadColumn(14)]
-        public float attr14 { get; set; }
+        public float attr14;
 
         [LoadColumn(15)]
-        public float attr15 { get; set; }
+        public float attr15;
 
         [LoadColumn(16)]
-        public float attr16 { get; set; }
+        public float attr16;
 
         [LoadColumn(17)]
-        public float attr17 { get; set; }
+        public float attr17;
 
         [LoadColumn(18)]
-        public float attr18 { get; set; }
+        public float attr18;
 
         [LoadColumn(19)]
-        public float attr19 { get; set; }
+        public float attr19;
 
         [LoadColumn(20)]
-        public float attr20 { get; set; }
+        public float attr20;
 
         [LoadColumn(21)]
-        public float attr21 { get; set; }
+        public float attr21;
 
         [LoadColumn(22)]
-        public float attr22 { get; set; }
+        public float attr22;
 
         [LoadColumn(23)]
-        public float attr23 { get; set; }
+        public float attr23;
 
         [LoadColumn(24)]
-        public float attr24 { get; set; }
+        public float attr24;
 
         [LoadColumn(25)]
-        public float attr25 { get; set; }
+        public float attr25;
 
         [LoadColumn(26)]
-        public float attr26 { get; set; }
+        public float attr26;
 
         [LoadColumn(27)]
-        public float attr27 { get; set; }
+        public float attr27;
 
         [LoadColumn(28)]
-        public float attr28 { get; set; }
+        public float attr28;
 
         [LoadColumn(29)]
-        public float attr29 { get; set; }
+        public float attr29;
 
         [LoadColumn(30)]
-        public float attr30 { get; set; }
+        public float attr30;
 
         [LoadColumn(31)]
-        public float attr31 { get; set; }
+        public float attr31;
 
         [LoadColumn(32)]
-        public float attr32 { get; set; }
+        public float attr32;
 
         [LoadColumn(33)]
-        public float attr33 { get; set; }
+        public float attr33;
 
         [LoadColumn(34)]
-        public float attr34 { get; set; }
+        public float attr34;
 
         [LoadColumn(35)]
-        public float attr35 { get; set; }
+        public float attr35;
 
         [LoadColumn(36)]
-        public float attr36 { get; set; }
+        public float attr36;
 
         [LoadColumn(37)]
-        public float attr37 { get; set; }
+        public float attr37;
 
         [LoadColumn(38)]
-        public float attr38 { get; set; }
+        public float attr38;
 
         [LoadColumn(39)]
-        public float attr39 { get; set; }
+        public float attr39;
 
         [LoadColumn(40)]
-        public float attr40 { get; set; }
+        public float attr40;
 
         [LoadColumn(41)]
-        public float attr41 { get; set; }
+        public float attr41;
 
         [LoadColumn(42)]
-        public float attr42 { get; set; }
+        public float attr42;
 
         [LoadColumn(43)]
-        public float attr43 { get; set; }
+        public float attr43;
 
         [LoadColumn(44)]
-        public float attr44 { get; set; }
+        public float attr44;
 
         [LoadColumn(45)]
-        public float attr45 { get; set; }
+        public float attr45;
 
         [LoadColumn(46)]
-        public float attr46 { get; set; }
+        public float attr46;
 
         [LoadColumn(47)]
-        public float attr47 { get; set; }
+        public float attr47;
 
         [LoadColumn(48)]
-        public float attr48 { get; set; }
+        public float attr48;
 
         [LoadColumn(49)]
-        public float attr49 { get; set; }
+        public float attr49;
 
         [LoadColumn(50)]
-        public float attr50 { get; set; }
+        public float attr50;
 
         [LoadColumn(51)]
-        public float attr51 { get; set; }
+        public float attr51;
 
         [LoadColumn(52)]
-        public float attr52 { get; set; }
+        public float attr52;
 
         [LoadColumn(53)]
-        public float attr53 { get; set; }
+        public float attr53;
 
         [LoadColumn(54)]
-        public float attr54 { get; set; }
+        public float attr54;
 
         [LoadColumn(55)]
-        public float attr55 { get; set; }
+        public float attr55;
 
         [LoadColumn(56)]
-        public float attr56 { get; set; }
+        public float attr56;
 
         [LoadColumn(57)]
-        public float attr57 { get; set; }
+        public float attr57;
 
         [LoadColumn(58)]
-        public float attr58 { get; set; }
+        public float attr58;
 
         [LoadColumn(59)]
-        public float attr59 { get; set; }
+        public float attr59;
 
         [LoadColumn(60)]
-        public float attr60 { get; set; }
+        public float attr60;
 
         [LoadColumn(61)]
-        public float attr61 { get; set; }
+        public float attr61;
 
         [LoadColumn(62)]
-        public float attr62 { get; set; }
+        public float attr62;
 
         [LoadColumn(63)]
-        public float attr63 { get; set; }
+        public float attr63;
 
         [LoadColumn(64)]
-        public float attr64 { get; set; }
+        public float attr64;
 
         [LoadColumn(65)]
-        public float attr65 { get; set; }
+        public float attr65;
 
         [LoadColumn(66)]
-        public float attr66 { get; set; }
+        public float attr66;
 
         [LoadColumn(67)]
-        public float attr67 { get; set; }
+        public float attr67;
 
         [LoadColumn(68)]
-        public float attr68 { get; set; }
+        public float attr68;
 
         [LoadColumn(69)]
-        public float attr69 { get; set; }
+        public float attr69;
 
         [LoadColumn(70)]
-        public float attr70 { get; set; }
+        public float attr70;
 
         [LoadColumn(71)]
-        public float attr71 { get; set; }
+        public float attr71;
 
         [LoadColumn(72)]
-        public float attr72 { get; set; }
+        public float attr72;
 
         [LoadColumn(73)]
-        public float attr73 { get; set; }
+        public float attr73;
 
         [LoadColumn(74)]
-        public float attr74 { get; set; }
+        public float attr74;
 
         [LoadColumn(75)]
-        public float attr75 { get; set; }
+        public float attr75;
 
         [LoadColumn(76)]
-        public float attr76 { get; set; }
+        public float attr76;
 
         [LoadColumn(77)]
-        public float attr77 { get; set; }
+        public float attr77;
 
         [LoadColumn(78)]
-        public float attr78 { get; set; }
+        public float attr78;
 
         [LoadColumn(79)]
-        public float attr79 { get; set; }
+        public float attr79;
 
         [LoadColumn(80)]
-        public float attr80 { get; set; }
+        public float attr80;
 
         [LoadColumn(81)]
-        public float attr81 { get; set; }
+        public float attr81;
 
         [LoadColumn(82)]
-        public float attr82 { get; set; }
+        public float attr82;
 
         [LoadColumn(83)]
-        public float attr83 { get; set; }
+        public float attr83;
 
         [LoadColumn(84)]
-        public float attr84 { get; set; }
+        public float attr84;
 
         [LoadColumn(85)]
-        public float attr85 { get; set; }
+        public float attr85;
 
         [LoadColumn(86)]
-        public float attr86 { get; set; }
+        public float attr86;
 
         [LoadColumn(87)]
-        public float attr87 { get; set; }
+        public float attr87;
 
         [LoadColumn(88)]
-        public float attr88 { get; set; }
+        public float attr88;
 
         [LoadColumn(89)]
-        public float attr89 { get; set; }
+        public float attr89;
 
         [LoadColumn(90)]
-        public float attr90 { get; set; }
+        public float attr90;
 
         [LoadColumn(91)]
-        public float attr91 { get; set; }
+        public float attr91;
 
         [LoadColumn(92)]
-        public float attr92 { get; set; }
+        public float attr92;
 
         [LoadColumn(93)]
-        public float attr93 { get; set; }
+        public float attr93;
 
         [LoadColumn(94)]
-        public float attr94 { get; set; }
+        public float attr94;
 
         [LoadColumn(95)]
-        public float attr95 { get; set; }
+        public float attr95;
 
         [LoadColumn(96)]
-        public float attr96 { get; set; }
+        public float attr96;
 
         [LoadColumn(97)]
-        public float attr97 { get; set; }
+        public float attr97;
 
         [LoadColumn(98)]
-        public float attr98 { get; set; }
+        public float attr98;
 
         [LoadColumn(99)]
-        public float attr99 { get; set; }
+        public float attr99;
 
         [LoadColumn(100)]
-        public float attr100 { get; set; }
+        public float attr100;
 
         [LoadColumn(101)]
-        public float attr101 { get; set; }
+        public float attr101;
 
         [LoadColumn(102)]
-        public float attr102 { get; set; }
+        public float attr102;
 
         [LoadColumn(103)]
-        public float attr103 { get; set; }
+        public float attr103;
 
         [LoadColumn(104)]
-        public float attr104 { get; set; }
+        public float attr104;
 
         [LoadColumn(105)]
-        public float attr105 { get; set; }
+        public float attr105;
 
         [LoadColumn(106)]
-        public float attr106 { get; set; }
+        public float attr106;
 
         [LoadColumn(107)]
-        public float attr107 { get; set; }
+        public float attr107;
 
         [LoadColumn(108)]
-        public float attr108 { get; set; }
+        public float attr108;
 
         [LoadColumn(109)]
-        public float attr109 { get; set; }
+        public float attr109;
 
         [LoadColumn(110)]
-        public float attr110 { get; set; }
+        public float attr110;
 
         [LoadColumn(111)]
-        public float attr111 { get; set; }
+        public float attr111;
 
         [LoadColumn(112)]
-        public float attr112 { get; set; }
+        public float attr112;
 
         [LoadColumn(113)]
-        public float attr113 { get; set; }
+        public float attr113;
 
         [LoadColumn(114)]
-        public float attr114 { get; set; }
+        public float attr114;
 
         [LoadColumn(115)]
-        public float attr115 { get; set; }
+        public float attr115;
 
         [LoadColumn(116)]
-        public float attr116 { get; set; }
+        public float attr116;
 
         [LoadColumn(117)]
-        public float attr117 { get; set; }
+        public float attr117;
 
         [LoadColumn(118)]
-        public float attr118 { get; set; }
+        public float attr118;
 
         [LoadColumn(119)]
-        public float attr119 { get; set; }
+        public float attr119;
 
         [LoadColumn(120)]
-        public float attr120 { get; set; }
+        public float attr120;
 
         [LoadColumn(121)]
-        public float attr121 { get; set; }
+        public float attr121;
 
         [LoadColumn(122)]
-        public float attr122 { get; set; }
+        public float attr122;
 
         [LoadColumn(123)]
-        public float attr123 { get; set; }
+        public float attr123;
 
         [LoadColumn(124)]
-        public float attr124 { get; set; }
+        public float attr124;
 
         [LoadColumn(125)]
-        public float attr125 { get; set; }
+        public float attr125;
 
         [LoadColumn(126)]
-        public float attr126 { get; set; }
+        public float attr126;
 
         [LoadColumn(127)]
-        public float attr127 { get; set; }
+        public float attr127;
 
         [LoadColumn(128)]
-        public float attr128 { get; set; }
+        public float attr128;
 
         [LoadColumn(129)]
-        public float attr129 { get; set; }
+        public float attr129;
 
         [LoadColumn(130)]
-        public float attr130 { get; set; }
+        public float attr130;
 
         [LoadColumn(131)]
-        public float attr131 { get; set; }
+        public float attr131;
 
         [LoadColumn(132)]
-        public float attr132 { get; set; }
+        public float attr132;
 
         [LoadColumn(133)]
-        public float attr133 { get; set; }
+        public float attr133;
 
         [LoadColumn(134)]
-        public float attr134 { get; set; }
+        public float attr134;
 
         [LoadColumn(135)]
-        public float attr135 { get; set; }
+        public float attr135;
 
         [LoadColumn(136)]
-        public float attr136 { get; set; }
+        public float attr136;
 
         [LoadColumn(137)]
-        public float attr137 { get; set; }
+        public float attr137;
 
         [LoadColumn(138)]
-        public float attr138 { get; set; }
+        public float attr138;
 
         [LoadColumn(139)]
-        public float attr139 { get; set; }
+        public float attr139;
 
         [LoadColumn(140)]
-        public float attr140 { get; set; }
+        public float attr140;
 
         [LoadColumn(141)]
-        public float attr141 { get; set; }
+        public float attr141;
 
         [LoadColumn(142)]
-        public float attr142 { get; set; }
+        public float attr142;
 
         [LoadColumn(143)]
-        public float attr143 { get; set; }
+        public float attr143;
 
         [LoadColumn(144)]
-        public float attr144 { get; set; }
+        public float attr144;
 
         [LoadColumn(145)]
-        public float attr145 { get; set; }
+        public float attr145;
 
         [LoadColumn(146)]
-        public float attr146 { get; set; }
+        public float attr146;
 
         [LoadColumn(147)]
-        public float attr147 { get; set; }
+        public float attr147;
 
         [LoadColumn(148)]
-        public float attr148 { get; set; }
+        public float attr148;
 
         [LoadColumn(149)]
-        public float attr149 { get; set; }
+        public float attr149;
 
         [LoadColumn(150)]
-        public float attr150 { get; set; }
+        public float attr150;
     }
 }
